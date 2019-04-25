186e1dd0b9335284142afbc027f2fd6c2b27fedc
Chris Mutel <cmutel@gmail.com>
2019-04-25 15:10:22 +0200

    Add initial library skeleton

---
+++ README.md
@@ -0,0 +1,17 @@
+# Coarse
+
+A parameterized Python model to calculate the energy consumption and life cycle assessment of cars.
+
+Extracted from (paper).
+
+* For readthedocs, click on the info button next to the badge in https://readthedocs.org/projects/whatever/, and copy the markdown code
+* For travis, click on the badge in https://travis-ci.org/BONSAMURAIS/whatever, and copy the markdown code
+* For appveyor, go to the projects settings page, and then click on "badges", and copy the markdown code
+
+## Installation
+
+Details on how to install the package
+
+## Contributing
+
+Details on how to contribute
---
+++ setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup
+import os
+
+packages = []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+
+# Probably should be changed, __init__.py is no longer required for Python 3
+for dirpath, dirnames, filenames in os.walk('coarse'):
+    # Ignore dirnames that start with '.'
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+setup(
+    name='coarse',
+    version="0.0.1",
+    packages=packages,
+    author="Chris Mutel",
+    author_email="cmutel@gmail.com",
+    license=open('LICENSE').read(),
+    # Only if you have non-python data (CSV, etc.). Might need to change the directory name as well.
+    # package_data={'your_name_here': package_files(os.path.join('coarse', 'data'))},
+    install_requires=[
+        'pandas',
+        'numpy',
+    ],
+    url="https://github.com/cmutel/coarse",
+    long_description=open('README.md').read(),
+    description='your_name_here',
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
