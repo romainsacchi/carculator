72cf97812317dd40b07570c5e42a14088ba306b1
Chris Mutel <cmutel@gmail.com>
2019-05-20 23:34:19 +0200

    Continue work on basic car model

--- coarse/__init__.py
+++ coarse/__init__.py
@@ -1,5 +1,6 @@
 __all__ = (
     "get_standard_driving_cycle",
+    "CarInputParameters",
 )
 __version__ = (0, 0, 1)
 
@@ -10,3 +11,4 @@ DATA_DIR = Path(__file__).resolve().parent / "data"
 
 
 from .driving_cycles import get_standard_driving_cycle
+from .car_input_parameters import CarInputParameters
