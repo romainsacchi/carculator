58ca7e30b859ee57113dbd98dd3a58a3a7f75741
romainsacchi <r_s@me.com>
2019-06-13 17:55:50 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['autoapi.extension']
+extensions = ['sphinx.ext.autodoc']
 
 autoapi_type = 'python'
 autoapi_dirs = ['../coarse']
