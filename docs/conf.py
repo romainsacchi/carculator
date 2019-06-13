7c6c93ebcf866e8ff8f8de65b34086bb358ac70e
romainsacchi <r_s@me.com>
2019-06-13 16:33:07 +0200

    Added documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -30,10 +30,10 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['autoapi.extension']
+#extensions = ['autoapi.extension']
 
-autoapi_type = 'python'
-autoapi_dirs = ['../coarse']
+#autoapi_type = 'python'
+#autoapi_dirs = ['../coarse']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
