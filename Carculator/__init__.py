699bcd905f44d001ae9dff383b6b91e1874f093d
romainsacchi <r_s@me.com>
2019-07-11 17:34:40 +0200

    Solve imports

--- Carculator/__init__.py
+++ Carculator/__init__.py
@@ -19,6 +19,9 @@ __all__ = (
 )
 __version__ = (0, 0, 1)
 
+# For relative imports to work in Python 3.6
+import os, sys;
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 from pathlib import Path
 
