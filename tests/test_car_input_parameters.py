ac042b5ce6b6d65e7cd14c0a83ad3d3922c04636
romainsacchi <r_s@me.com>
2019-07-12 15:49:27 +0200

    Update test_car_input_parameters.py

--- tests/test_car_input_parameters.py
+++ tests/test_car_input_parameters.py
@@ -8,5 +8,5 @@ def test_type_parameters():
     assert wrapped_error.value.code ==1
 
 def test_retrieve_list_powertrains():
-    assert isinstance(cip.CarInputParameters().powertrains, dict)
+    assert isinstance(cip.CarInputParameters().powertrains, list)
     assert len(cip.CarInputParameters().powertrains>0)
