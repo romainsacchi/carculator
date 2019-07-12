d1a44d076ade1f251e5a03361c11ee87e839a135
romainsacchi <r_s@me.com>
2019-07-12 13:21:34 +0200

    Update test_driving_cycles.py

--- tests/test_driving_cycles.py
+++ tests/test_driving_cycles.py
@@ -1,6 +1,6 @@
 import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/carculator/')
+sys.path.insert('/Users/travis/build/romainsacchi/carculator/carculator/')
 
 print(sys.path)
 
