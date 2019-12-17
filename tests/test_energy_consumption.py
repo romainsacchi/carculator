bd0ef18ae6ae371b43fb78949f8ab74eb32fd8d8
romainsacchi <r_s@me.com>
2019-12-17 14:55:55 +0100

    Added tests

--- tests/test_energy_consumption.py
+++ tests/test_energy_consumption.py
@@ -1,6 +1,5 @@
 from carculator.energy_consumption import EnergyConsumptionModel
 
-
 def test_acceleration():
     ecm = EnergyConsumptionModel('WLTC')
     assert ecm.acceleration.sum() == 0
@@ -12,13 +11,13 @@ def test_aux_power():
 
 def test_motive_energy():
     ecm = EnergyConsumptionModel('WLTC')
+    aux = ecm.aux_energy_per_km(93.75)
+    aux /= 0.29
     motive = ecm.motive_energy_per_km(
-        driving_mass=1800,
-        rr_coef=0.05,
-        drag_coef=0.01,
-        frontal_area=2,
-        ttw_efficiency=0.1).sum()
-    assert int(motive) == 9615
-
-
-
+        driving_mass=1879,
+        rr_coef=0.01,
+        drag_coef=0.3,
+        frontal_area=2.4,
+        ttw_efficiency=0.232).sum()
+    assert int(motive + aux) > 2500
+    assert int(motive + aux) < 2700
