d5d9d9db1d55a779484f9578e2f70e961632001a
romainsacchi <r_s@me.com>
2019-12-18 12:00:31 +0100

    Update test_driving_cycles.py

--- tests/test_driving_cycles.py
+++ tests/test_driving_cycles.py
@@ -1,18 +1,18 @@
-import pandas
 import pytest
+import numpy as np
 from carculator.driving_cycles import get_standard_driving_cycle
 
 
 def test_cycle_retrieval_default():
     dc = get_standard_driving_cycle()
-    assert isinstance(dc, pandas.core.series.Series)
+    assert isinstance(dc, np.ndarray)
     assert dc.sum() == 83744.6
     assert dc.index.min() == 0
     assert dc.index.max() == 1800
 
 def test_cycle_retrieval_wltc():
     dc = get_standard_driving_cycle("WLTC")
-    assert isinstance(dc, pandas.core.series.Series)
+    assert isinstance(dc, np.ndarray)
     assert dc.sum() == 83744.6
     assert dc.index.min() == 0
     assert dc.index.max() == 1800
@@ -20,14 +20,14 @@ def test_cycle_retrieval_wltc():
 
 def test_cycle_retrieval_nedc():
     dc = get_standard_driving_cycle("NEDC")
-    assert isinstance(dc, pandas.core.series.Series)
+    assert isinstance(dc, np.ndarray)
     assert dc.sum() == 39353.0
     assert dc.index.min() == 0
     assert dc.index.max() == 1200
 
 def test_cycle_retrieval_cadc():
     dc = get_standard_driving_cycle("CADC")
-    assert isinstance(dc, pandas.core.series.Series)
+    assert isinstance(dc, np.ndarray)
     assert dc.sum() == 186074.2
     assert dc.index.min() == 0
     assert dc.index.max() == 3143
