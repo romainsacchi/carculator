e2c959b357a469fcdc57a7ab7ce45dbfaa26ded6
romainsacchi <r_s@me.com>
2019-12-19 12:26:11 +0100

    Update inventory.py

--- carculator/inventory.py
+++ carculator/inventory.py
@@ -1255,15 +1255,14 @@ class InventoryCalculation:
             * -1
         ).transpose([1,0,2])
 
-        if cng_technology is not None:
+        if 'cng_technology' in locals():
             if cng_technology == 'biogas':
                 # change fossil emissions to non-fossil, if first iteration
                 self.A[:,
                     self.inputs[('Carbon dioxide, fossil', ('air',), 'kilogram')],
                     self.index_cng] = 0
 
-
-        if diesel_technology is not None:
+        if 'diesel_technology' in locals():
             if diesel_technology in ('biodiesel - algae', 'biodiesel - cooking oil'):
                 # change fossil emissions to non-fossil, if first iteration
 
@@ -1271,7 +1270,7 @@ class InventoryCalculation:
                     self.inputs[('Carbon dioxide, fossil', ('air',), 'kilogram')],
                     self.index_diesel] = 0
 
-        if petrol_technology is not None:
+        if 'petrol_technology' in locals():
             if petrol_technology in ('bioethanol - wheat straw', 'bioethanol - forest residues',
                                  'bioethanol - sugarbeet', 'bioethanol - maize starch'):
             # change fossil emissions to non-fossil, if first iteration
