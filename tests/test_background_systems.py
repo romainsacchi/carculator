bf9c22d8284068375df6db7580fc087acf8c033c
romainsacchi <r_s@me.com>
2020-04-21 18:26:57 +0200

    Update test_background_systems.py

--- tests/test_background_systems.py
+++ tests/test_background_systems.py
@@ -5,7 +5,7 @@ import pytest
 def test_size_dictionary():
     bs = BackgroundSystemModel()
     assert len(bs.electricity_mix) == 89
-    assert int(bs.electricity_mix.sel(country="FR").values.sum()) == 7
+    assert int(bs.electricity_mix.sel(country="FR").values.sum()) == 10
     assert int(bs.electricity_mix.sel(country="FR", year=2015).values.sum()) == 1
 
 
