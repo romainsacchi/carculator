e3f8621c9a8daf4704a24fd920d73a751d4282df
romainsacchi <romain@Romains-iMac.psi.ch>
2024-01-23 14:32:39 +0100

    Update dependency

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     license=open("LICENSE").read(),
     package_data={"carculator": package_files(os.path.join("carculator", "data"))},
     install_requires=[
-        "carculator_utils==1.2.0.dev0",
+        "carculator_utils==1.2.0.dev1",
     ],
     url="https://github.com/romainsacchi/carculator",
     description="Prospective life cycle assessment of vehicles made blazing fast",
